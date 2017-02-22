31bd30336cfe14c09a0273c9303e0e27543b6375
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2017-02-22 22:05:06 +0700

    edit Json send data to mqtt

--- ARD-3G-GPS-SD/ARD-3G-GPS-SD.ino
+++ ARD-3G-GPS-SD/ARD-3G-GPS-SD.ino
@@ -41,10 +41,10 @@ void debug(String data) {
 
 void setup()  {
   byte _hour, _minute, _second, _day, _month, _year, _batt;
-  byte _volume, _temp, _humid, _pitch, _roll;
+  byte _binID, _volume, _temp, _humid, _pitch, _roll;
   int _carbon, _methane, _press, _alt;
   word _light;
-  float _lat, _lon;
+  //  float _lat, _lon;
   boolean _lidStatus, _flameStatus, _soundStatus;
 
   Serial.begin(9600);
@@ -87,7 +87,9 @@ void setup()  {
 
   for (int i = 0; i < 6; i++) {
     gps_data = gps.GetNMEA("GGA");
+    digitalWrite(6, 1);
     delay(3000);
+    digitalWrite(6, 0);
     //    Serial.print(F("GGA get : "));
     //    Serial.println(gps_data);
   }
@@ -142,14 +144,56 @@ void setup()  {
   ////////////////////////////JSON////////////////////////////
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.createObject();
+  root["stat"];
   root["data"];
+  root["loca"];
+
+  _binID = 1;
+  _volume = 5;
+  _lidStatus = true;
+  _temp = 25;
+  _humid  = 40;
+  _flameStatus = true;
+  _soundStatus = false;
+  _carbon = 5000;
+  _methane = 5000;
+  _light = 150;
+  _pitch = 90;
+  _roll = 90;
+  _press = 10000;
+  _batt = 100;
+  _hour = 18;
+  _minute = 55;
+  _second = 30;
+  _day = 22;
+  _month = 2;
+  _year = 2017;
+
+  JsonArray& stat = root.createNestedArray("stat");
+  stat.add(String("binId:" + String(_binID)));
+  stat.add(String("volum:" + String(_volume)));
+  stat.add(String("lid:"  + String(_lidStatus)));
+  stat.add(String("flame:"  + String(_flameStatus)));
+  stat.add(String("sound:"  + String(_soundStatus)));
+  stat.add(String("pitch:" + String(_pitch)));
+  stat.add(String("roll:" + String(_roll)));
   JsonArray& data = root.createNestedArray("data");
-  data.add(String("location:" + gps_lat + "," + gps_lon + "," + gps_alt));
-  //  data.add(gps_lat);
-  //  data.add("lon");
-  //  data.add(gps_lon);
-  //  data.add("alt");
-  //  data.add(gps_alt);
+  data.add(String("temp:"  + String(_temp)));
+  data.add(String("huid:" + String(_humid)));
+  data.add(String("carbon:" + String(_carbon)));
+  data.add(String("methane:" + String(_methane)));
+  data.add(String("light:" + String(_light)));
+  data.add(String("press:" + String(_press)));
+  data.add(String("batt:" + String(_batt)));
+  JsonArray& loca = root.createNestedArray("loca");
+  loca.add(String("location" + gps_lat + "," + gps_lon + "," + gps_alt));
+
+  //  data.add(String("hour:" + String(_hour)));
+  //  data.add(String("minute:"  + String(_minute)));
+  //  data.add(String("second:" + String(_second)));
+  //  data.add(String("day:" + String(_day)));
+  //  data.add(String("month:"  + String(_month)));
+  //  data.add(String("year:" + String(_year)));
   root.printTo(Serial);
   ////////////////////////////SDCARD////////////////////////////
   //  card.begin(CHIP_SELECT);
@@ -166,8 +210,6 @@ void setup()  {
   //  file.close();
   //  delay(2000);
   //////////////////////////////MQTT////////////////////////////////
-  //  mqtt.callback = callback;
-  //  connect_server();
   do  {
     Serial.println(F("Connect Server"));
     Serial.println(F("wait connect"));
@@ -183,10 +225,17 @@ void setup()  {
   unsigned char ret = mqtt.Connect(MQTT_ID, MQTT_USER, MQTT_PASSWORD);
   Serial.println(mqtt.ConnectReturnCode(ret));
   digitalWrite(6, 1);
+  mqtt.Publish("/SmartTrash/gearname/status", root["stat"], false);
+  delay(1000);
   mqtt.Publish("/SmartTrash/gearname/data", root["data"], false);
   delay(1000);
+  mqtt.Publish("/SmartTrash/gearname/location", root["loca"], false);
+  delay(1000);
   digitalWrite(6, 0);
+  //  mqtt.callback = callback;
+  //  connect_server();
 }
+
 
 //void callback(String topic , char *playload, unsigned char length)  {
 //  Serial.println();
